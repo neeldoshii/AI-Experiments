7311d3e14d3c7cbb41bc0bf4427ac06f4efb8d01
Neel Doshi <neeldoshi147@gmail.com>
2024-04-29 01:18:41 +0530

    refactored traveling sales person

--- travelingsalesperson.py
+++ travelingsalesperson.py
@@ -1,29 +1,24 @@
-from sys import maxsize
 from itertools import permutations
-V = 4
 
-def travellingSalesmanProblem(graph, s):
-	vertex = []
-	for i in range(V):
-		if i != s:
-			vertex.append(i)
+NUM_CITIES = 4
 
-	min_path = maxsize
-	next_permutation=permutations(vertex)
-	for i in next_permutation:
-		current_pathweight = 0
-		k = s
-		for j in i:
-			current_pathweight += graph[k][j]
-			k = j
-		current_pathweight += graph[k][s]
-		min_path = min(min_path, current_pathweight)
-		
-	return min_path
+def travelling_salesman(graph, start):
+    cities = [i for i in range(NUM_CITIES) if i != start]
+
+    min_path = 99999
+    for i in permutations(cities):
+        current_path = 0
+        current_city = start
+        for next_city in i:
+            current_path += graph[current_city][next_city]
+            current_city = next_city
+        current_path += graph[current_city][start]
+        min_path = min(min_path, current_path)
+        
+    return min_path
 
 
-if __name__ == "__main__":
-	graph = [[0, 10, 15, 20], [10, 0, 35, 25],
-			[15, 35, 0, 30], [20, 25, 30, 0]]
-	s = 0
-	print(travellingSalesmanProblem(graph, s))
+graph = [[0, 10, 15, 20], [10, 0, 35, 25],[15, 35, 0, 30], [20, 25, 30, 0]]
+starting_city = 0
+print("The cost of most efficient tour is ", end ="")
+print(travelling_salesman(graph, starting_city))
