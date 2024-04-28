6bfb3d43743ee441242e14742357f079627e194a
Neel Doshi <neeldoshi147@gmail.com>
2024-04-28 23:03:39 +0530

    Refactored BFS to support user input for dynamic support.

--- bfs.py
+++ bfs.py
@@ -1,28 +1,28 @@
-# Adjacency Matrix
-graphs = {
-    '5' : ['3','7'],
-    '3' : ['2','4'],
-    '7' : ['8'],
-    '2' : [],
-    '4' : ['8'],
-    '8' : []
-}
+def create_graph():
+    graphs = {}
+    nodes = int(input("Enter the number of nodes: "))
+    for _ in range(nodes):
+        node = input("Enter node: ")
+        neighbors_input = input("Enter neighbors (comma-separated): ")
+        neighbors = neighbors_input.split(',') if neighbors_input else []
+        graphs[node] = neighbors
+    return graphs
 
-visited = [] # contains the list of visited nodes 
-queue = [] # queue of nodes
-
-def bfs(visited, graphs, nodes):
-    visited.append(nodes)
-    queue.append(nodes)
+def bfs(visited, graphs, start_node):
+    visited.append(start_node)
+    queue = [start_node] # queue of nodes
 
     while queue:
-        m = queue.pop(0) # takes the first element of the queue 
-        print(m, end= " ")
+        current_node = queue.pop(0) # takes the first element of the queue 
+        print(current_node, end=" ")
 
-        for neighbor in graphs[m]:
+        for neighbor in graphs[current_node]:
             if neighbor not in visited:
                 visited.append(neighbor)
                 queue.append(neighbor)
 
 print("Breadth First Search")
-bfs(visited,graphs,'5')
+graphs = create_graph()
+start_node = input("Enter the starting node for BFS: ")
+visited = [] # contains the list of visited nodes 
+bfs(visited, graphs, start_node)
