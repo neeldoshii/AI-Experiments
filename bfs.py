2f3c173c4933b2e0fb2a176cb83ff1079c7e76a8
Neel Doshi <neeldoshi147@gmail.com>
2024-04-28 23:49:29 +0530

    Simplified

--- bfs.py
+++ bfs.py
@@ -1,7 +1,7 @@
 def create_graph():
     graphs = {}
     nodes = int(input("Enter the number of nodes: "))
-    for _ in range(nodes):
+    for i in range(nodes):
         node = input("Enter node: ")
         neighbors_input = input("Enter neighbors (comma-separated): ")
         neighbors = neighbors_input.split(',') if neighbors_input else []
